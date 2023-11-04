b3dd4113b0c9bc6ed951bae7580a6d7006188954
pgk95 <kalupgk@gmail.com>
2023-11-04 13:05:49 +0300

    encrypt password tasks

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""password encryption"""
+
+import bcrypt
+
+
+def hash_password(password) -> bytes:
+    """returns a hashed password, which is a byte string"""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if a password is valid"""
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
