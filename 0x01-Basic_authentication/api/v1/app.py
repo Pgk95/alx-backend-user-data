5de9126478f20729a2fa217b868b9d6f283a75ce
pgk95 <kalupgk@gmail.com>
2023-11-08 20:56:37 +0300

    task 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,9 +22,15 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def Unauthorized(error) -> str:
-    """unauthorized handler"""
+def unauthorized(error) -> str:
+    """unauthorized error handler"""
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """forbidden error handler"""
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
