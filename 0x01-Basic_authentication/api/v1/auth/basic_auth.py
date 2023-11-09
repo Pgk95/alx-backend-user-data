b59873c0b86de5688c06c4a58c46004b238e8ea3
pgk95 <kalupgk@gmail.com>
2023-11-09 11:18:46 +0300

    quick fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,8 +42,8 @@ class BasicAuth(Auth):
             return None, None
         return tuple(decoded_base64_authorization_header.split(':', 1))
 
-    def user_object_from_credentials(self, user_email: str, 
-                                         user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(self, user_email:
+                                     str, user_pwd: str) -> TypeVar('User'):
         """returns the user instance based
            on his email and password
         """
