4e3b50db4e56c7dddbc5e64e81ce7b67728fc480
pgk95 <kalupgk@gmail.com>
2023-11-09 20:18:34 +0300

    quick fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,7 +24,7 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None):
         """returns a user based on a cookie value"""
         session_id = self.session_cookie(request)
