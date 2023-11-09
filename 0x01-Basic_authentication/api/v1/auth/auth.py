9111ef8baeac0b8fe5d5c73c5f9b2ff9003a1f7f
pgk95 <kalupgk@gmail.com>
2023-11-09 11:38:13 +0300

    quick fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,15 +14,18 @@ class Auth:
             return True
         if path[-1] != '/':
             path += '/'
-        if path in excuded_paths:
+        if excluded_paths[-1] != '/':
+            excluded_paths += '/'
 
-            astericks = [stars[:-1]
-                         for stars in excuded_paths if stars[-1] == '*']
-        if path in astericks:
-            return False
-        for star in astericks:
-            if path.startswith(star):
+        astericks = [stars[:-1]
+                     for stars in excluded_paths if stars[-1] == '*']
+
+        for stars in astericks:
+            if path.startswith(stars):
                 return False
+
+        if path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
