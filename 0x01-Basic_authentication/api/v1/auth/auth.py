c7a53d65c359ada046ed862fdfa8927c70847366
pgk95 <kalupgk@gmail.com>
2023-11-09 09:43:01 +0300

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,6 +22,10 @@ class Auth:
         """Authorization header"""
         if request is None:
             return None
+        if 'Authorization' not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user"""
