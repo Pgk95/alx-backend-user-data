b8d1e9d28fa2484e1d9488cb624b17dafe2051b4
pgk95 <kalupgk@gmail.com>
2023-11-16 11:32:43 +0300

    task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,6 +23,16 @@ class Auth:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """valid login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """Hash password
