692e9c904f6d685214bfef833aacd526d5b77470
pgk95 <kalupgk@gmail.com>
2023-11-16 13:20:45 +0300

    quick fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,10 +47,13 @@ def login() -> str:
 def logout():
     """logout"""
     session_id = request.cookies.get('session_id')
-    if not (session_id or AUTH.get_user_from_session_id(session_id)):
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect('/')
+    else:
         abort(403)
-    AUTH.destroy_session(session_id)
-    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
