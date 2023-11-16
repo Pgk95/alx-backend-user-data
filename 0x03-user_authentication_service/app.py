5864e433a1d3579d6bd8d3cec044e9f5f8fc5b8c
pgk95 <kalupgk@gmail.com>
2023-11-16 12:39:59 +0300

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 from auth import Auth
+
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -15,6 +16,7 @@ def index() -> str:
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
+    """register"""
     if request.form.get('email') and request.form.get('password'):
         try:
             user = AUTH.register_user(request.form.get('email'),
@@ -22,6 +24,23 @@ def users() -> str:
             return jsonify({"email": user.email, "message": "user created"})
         except ValueError:
             return jsonify({"message": "email already registered"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """login"""
+    if request.form.get('email') and request.form.get('password'):
+        if AUTH.valid_login(request.form.get('email'),
+                            request.form.get('password')):
+            session_id = AUTH.create_session(request.form.get('email'))
+            response = jsonify({"email": request.form.get('email'),
+                                "message": "logged in"})
+            response.set_cookie('session_id', session_id)
+            return response
+        else:
+            abort(401)
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
